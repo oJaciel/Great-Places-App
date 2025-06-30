5ffc836b96bdb5c83c655d8369cb3795c3a56650
oJaciel <jacielhbarichellO@gmail.com>
2025-06-30 06:17:31 -0300

    Formulário Novo Lugar
    Implementado, ainda sem métodos, o formulário para cadastro de um novo lugar.
    
    Criado componente para trabalhar com as imagens da câmera.

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({super.key});
@@ -8,11 +9,48 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Novo Lugar')),
-      body: Center(child: Text('Form')),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(labelText: 'Título'),
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: _submitForm,
+              label: Text('Adicionar'),
+              icon: Icon(Icons.add),
+              style: ButtonStyle(
+                elevation: WidgetStatePropertyAll(0),
+                backgroundColor: WidgetStatePropertyAll(
+                  Theme.of(context).colorScheme.secondary,
+                ),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 180,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          alignment: Alignment.center,
+          child: Text('Nenhuma imagem!'),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: () {},
+            label: Text(
+              'Tirar Foto',
+              style: TextStyle(color: Theme.of(context).primaryColor),
+            ),
+            icon: Icon(Icons.camera),
+          ),
+        ),
+      ],
+    );
+  }
+}
