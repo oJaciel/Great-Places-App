d0cd6324033a2060082099062f45cdee3952b872
oJaciel <jacielhbarichellO@gmail.com>
2025-07-02 20:48:29 -0300

    Componente LocationInput + Package Location
    Criado componente para trabalhar com os dados de localização. *Ainda sem implementação
    
    Adicionado o package Location, e configurado para funcionamento no IOS.

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child:
+              _previewImageUrl == null
+                  ? Text('Localização  não informada')
+                  : Image.network(
+                    _previewImageUrl!,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Localização Atual',
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              icon: Icon(Icons.location_on),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Selecione no Mapa',
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              icon: Icon(Icons.map),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
